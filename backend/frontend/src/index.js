ac3277c01b4df2ee6f1353b168139e804fc887ef
Teddy <inteldev2080@gmail.com>
2021-01-05 13:51:23 +0530

    changes

--- backend/frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import App from "./components/App";
-import "font-awesome/css/font-awesome.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
-import $ from "jquery";
-import Popper from "popper.js";
-import "./styles.css";
--- backend/frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import blogs from "./blogs";
-import auth from "./auth";
-
-export default combineReducers({
-  blogs,
-  auth,
-});
